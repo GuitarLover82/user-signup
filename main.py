5b1c23c7f622f993204b98be227b68db2570336f
Mark William <markdwilliams82@gmail.com>
2018-09-25 15:58:58 -0500

    accounted for errors

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader
 (template_dir), autoescape=True)
 
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template('index.html')
+    return template.render()
+
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    user_name = request.form['user_name']
+    template = jinja_env.get_template('welcome.html')
+    return template.render(name=user_name)
 
 app.run() 
