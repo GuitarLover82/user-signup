75fdadd6cd0e19c1fb023c9ee72e0a7c2c8162de
Mark William <markdwilliams82@gmail.com>
2018-09-26 17:29:54 -0500

    not sure why my password won't clear when an email error occurs

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 from flask import Flask, request, redirect, render_template
-import cgi
-import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader
-(template_dir), autoescape=True)
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/")
 def index():
-    template = jinja_env.get_template('index.html')
-    return template.render()
+    return render_template('index.html')
 
 
-
-@app.route("/welcome", methods=['POST', 'GET'])
-def welcome():
-    template = jinja_env.get_template('welcome.html')
+@app.route("/", methods=['POST'])
+def signup():
     
     user_name = request.form['user_name']
     user_password = request.form['user_password']
@@ -32,39 +21,75 @@ def welcome():
     verify_password_error = ""
     email_error = ""
 
-    if user_name == "":
-        user_name_error = "That's not a valid user name"
-    elif " " in user_name:
-        user_name_error =  "That's not a valid user name"    
-    elif len(user_name) < 3 or len(user_name) >20:
-        user_name_error = "That's not a valid username" 
-
     if user_password == "":
         user_password_error = "That's not a valid password"
+        user_password = ""
+        verify_password = ""
     elif " " in user_password:
         user_password_error = "That's not a valid password"
+        user_password = ""
+        verify_password = ""
     elif len(user_password) < 3 or len(user_password) > 20:
         user_password_error = "That's not a valid password"
         user_password = ""
+        verify_password = ""
 
     if verify_password == "":
         verify_password_error = "passwords don't match"
+        verify_password = ""
+        user_password = ""
     elif verify_password != user_password:
         verify_password_error = "passwords don't match"
         verify_password = ""
+        user_password = ""
 
-    if " " in email:
+    if user_name == "":
+        user_name_error = "That's not a valid user name"
+        user_password = ""
+        verify_password = ""
+    elif " " in user_name:
+        user_name_error =  "That's not a valid user name"
+        user_password = ""
+        verify_password = ""    
+    elif len(user_name) < 3 or len(user_name) >20:
+        user_name_error = "That's not a valid username"
+        user_password = ""
+        verify_password = "" 
+
+    if email == "":
+        email_error == False
+    elif " " in email:
         email_error = "That is not a valid email"
+        user_password = ""
+        verify_password = ""
     elif "@" not in email or "." not in email:
         email_error = "That is not a valid email"
+        user_password = ""
+        verify_password = ""
+    elif email.count(".") > 1:
+        email_error = "That is not a valid email"
+        user_password = ""
+        verify_password = ""
+    elif email.count("@") > 1:
+        email_error = "That is not a valid email"
+        user_password = ""
+        verify_password = ""           
     elif len(email) < 3 or len(email) > 20:
         email_error = "That is not a valid email"
+        user_password = ""
+        verify_password = ""
 
     if not user_name_error and not user_password_error and not verify_password_error and not email_error:
-        return 
+        user_name = user_name
+        return redirect('/welcome?user_name={0}'.format(user_name))
     else:    
-        return template.render(user_name=user_name, user_name_error=user_name_error, user_password=user_password, 
+        return render_template('index.html', user_name=user_name, user_name_error=user_name_error, user_password=user_password, 
         user_password_error=user_password_error, verify_password=verify_password, verify_password_error=verify_password_error,
          email=email, email_error=email_error)
+
+@app.route("/welcome")
+def welcome():
+    user_name = request.args.get('user_name')
+    return render_template('welcome.html', user_name=user_name)    
 
 app.run() 
